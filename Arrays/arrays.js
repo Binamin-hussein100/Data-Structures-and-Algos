d4e23dcc9971093eb25f16e0492435039aedca79
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-15 07:59:47 +0300

    shift and unshift

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -5,5 +5,13 @@ nums.push(8)
 console.log(nums) // [ 1, 2, 4, 7, 8 ]
 
 // - pop() => Remove element from the end of array
-nums.pop()
+nums.pop() // returns the value that was removed // 8
 console.log(nums) // [ 1, 2, 4, 7 ]
+
+// - shift() => Subtract element from the beginning of array
+nums.shift()
+console.log(nums) // [ 2, 4, 7 ]
+
+// unshift() => Add element to the beginning of array
+nums.unshift(100)
+console.log(nums) // 
