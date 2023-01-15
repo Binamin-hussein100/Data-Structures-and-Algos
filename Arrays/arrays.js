caa6cc7765386d4984d95577805fd33dcd540a45
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-01-15 07:55:36 +0300

    pop

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,6 +1,9 @@
 const nums = [1,2,4,7]
 
 // - push() => Add element to the end of array
-
 nums.push(8)
 console.log(nums) // [ 1, 2, 4, 7, 8 ]
+
+// - pop() => Remove element from the end of array
+nums.pop()
+console.log(nums) // [ 1, 2, 4, 7 ]
